10f4c42a240cb15d24ba662c482ffb1d23310541
Tom Allen <tomjamesallenweb@gmail.com>
2019-03-15 16:20:21 +0000

    Removes jest deps from react-scripts

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -27,8 +27,7 @@ const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 switch (script) {
   case 'build':
   case 'eject':
-  case 'start':
-  case 'test': {
+  case 'start': {
     const result = spawn.sync(
       'node',
       nodeArgs
